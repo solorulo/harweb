6853e1359c17804cb20f3ab9c9e1c6c4cd227cc4
mr_marshal <mr_marshal@Marshal-PC>
2014-09-09 18:10:14 -0500

    Agregué el fragment de los espacios

--- waldenme/src/com/waldenme/fragments/SpaceFragment.java
+++ waldenme/src/com/waldenme/fragments/SpaceFragment.java
@@ -1,80 +1,45 @@
 package com.waldenme.fragments;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.waldenme.MainActivity;
 import com.waldenme.R;
 
-/**
- * A simple {@link android.support.v4.app.Fragment} subclass. Activities that
- * contain this fragment must implement the
- * {@link SpaceFragment.OnFragmentInteractionListener} interface to handle
- * interaction events. Use the {@link SpaceFragment#newInstance} factory method
- * to create an instance of this fragment.
- * 
- */
+@SuppressLint("ViewHolder")
 public class SpaceFragment extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_SECTION_NUMBER = "section_number";
-//	private static final String ARG_PARAM1 = "param1";
-//	private static final String ARG_PARAM2 = "param2";
-
-	// TODO: Rename and change types of parameters
-//	private String mParam1;
-//	private String mParam2;
+	private static final String ARG_SECTION_NUMBER = "section_number";
 
 	private OnFragmentInteractionListener mListener;
 
-	/**
-	 * Use this factory method to create a new instance of this fragment using
-	 * the provided parameters.
-	 * 
-	 * @param param1
-	 *            Parameter 1.
-	 * @param param2
-	 *            Parameter 2.
-	 * @return A new instance of fragment SpaceFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
-//	public static SpaceFragment newInstance(String param1, String param2) {
 	public static SpaceFragment newInstance(int sectionNumber) {
 		SpaceFragment fragment = new SpaceFragment();
 		Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//		args.putString(ARG_PARAM1, param1);
-//		args.putString(ARG_PARAM2, param2);
+		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
 		fragment.setArguments(args);
 		return fragment;
 	}
 
-	public SpaceFragment() {
-		// Required empty public constructor
-	}
+	public SpaceFragment() {}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-//			mParam1 = getArguments().getString(ARG_PARAM1);
-//			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
 	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_space, container, false);
-	}
-
-	// TODO: Rename method, update argument and hook method into UI event
 	public void onButtonPressed(Uri uri) {
 		if (mListener != null) {
 			mListener.onFragmentInteraction(uri);
@@ -84,14 +49,8 @@ public class SpaceFragment extends Fragment {
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-        ((MainActivity) activity).onSectionAttached(
-                getArguments().getInt(ARG_SECTION_NUMBER));
-//		try {
-//			mListener = (OnFragmentInteractionListener) activity;
-//		} catch (ClassCastException e) {
-//			throw new ClassCastException(activity.toString()
-//					+ " must implement OnFragmentInteractionListener");
-//		}
+		((MainActivity) activity).onSectionAttached(getArguments().getInt(
+				ARG_SECTION_NUMBER));
 	}
 
 	@Override
@@ -100,18 +59,44 @@ public class SpaceFragment extends Fragment {
 		mListener = null;
 	}
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated to
-	 * the activity and potentially other fragments contained in that activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
 	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
 		public void onFragmentInteraction(Uri uri);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_space, container,
+				false);
+
+		String[] datos = new String[] { "Sala de juntas","Área de trabajo",
+				"Estudio de diseño","Taller de herramientas",
+				"Sala de cómputo","Sala creativa","Sala de juntas","Otra sala de juntas"
+		};
+
+		ArrayList<String> list = new ArrayList<String>(Arrays.asList(datos));
+		setObj adapter = new setObj(getActivity(), 0, 0, list);
+		ListView lv = (ListView) rootView.findViewById(R.id.list);
+		lv.setAdapter(adapter);
+		return rootView;
+
+	}
+
+	public class setObj extends ArrayAdapter<String> {
+		Context ctx;	List<String> datos;	TextView txtView;	View rootV;
+		public setObj(Context context, int resource, int textViewResourceId,
+				List<String> objects) {
+			super(context, resource, textViewResourceId, objects);
+			datos = objects;	ctx = context;
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = LayoutInflater.from(ctx);
+			rootV = inflater.inflate(R.layout.simple_space_fragment_list_item, null);
+			txtView = (TextView) rootV.findViewById(android.R.id.text1);
+			txtView.setText(datos.get(position));
+			return rootV;
+		}
 	}
 
 }
